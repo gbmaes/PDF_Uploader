b5cfc453c3783a6397e9ffae93a49b6522fb1520
Gabriel Bessa <gabriel.bessa@solutions.valantic.com>
2023-07-27 10:59:18 +0200

    first commit

---
+++ ui5-pdf-viewer-master/webapp/controller/main.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+  "com/ui5/PDF_Uploader/controller/BaseController"
+], function (Controller) {
+  "use strict";
+
+  return Controller.extend("com.ui5.PDF_Uploader.controller.main", {
+    handleFileChange: function (oEvent) {
+      var file = oEvent.getParameter("files")[0];
+
+      var pdfViewer = this.getView().byId("pdfViewer");
+      this.getFileForPreview(file, function (source) {
+        if (source) {
+          pdfViewer.setPdfSource(source);
+          pdfViewer.setVisible(true);
+        }
+      });
+    },
+
+    getFileForPreview: function (file, callback) {
+      var reader = new FileReader();
+      reader.onload = function () {
+        callback(reader.result);
+      }
+      reader.onerror = function () {
+        callback(null);
+      }
+      reader.readAsDataURL(file);
+    }
+  });
+});
